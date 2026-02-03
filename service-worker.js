efc28c2b298c7c65736ec9579f53a2b273f797f7
anastasu989 <belocerkovecanastasia6@gmail.com>
2026-02-03 16:05:52 +0500

    Refactor background message handling in service worker

--- service-worker.js
+++ service-worker.js
@@ -5,20 +5,18 @@ firebase.initializeApp({
   apiKey: "AIzaSyDYjTq4Byv_HvpSymIq7myrdnvSCbetxc8",
   authDomain: "anastasia-b2c21.firebaseapp.com",
   projectId: "anastasia-b2c21",
-  storageBucket: "anastasia-b2c21.appspot.com",
   messagingSenderId: "219596761766",
   appId: "1:219596761766:web:aecd1431a8741559c3a267"
 });
 
 const messaging = firebase.messaging();
 
-messaging.onBackgroundMessage(payload => {
-  const title = payload.notification.title || "Новое сообщение";
-  const options = {
-    body: payload.notification.body,
-    icon: "/icon-192.png",
-    badge: "/icon-192.png"
-  };
-
-  self.registration.showNotification(title, options);
+messaging.onBackgroundMessage(function(payload) {
+  self.registration.showNotification(
+    payload.notification.title,
+    {
+      body: payload.notification.body,
+      icon: "/icon-192.png"
+    }
+  );
 });
